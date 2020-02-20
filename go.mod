ef6ec2fbc4d807eabe585d2f3c9d6ba6f01cec39
Charles Vandevoorde <charles@vandevoorde.me>
2020-02-20 21:46:43 +0100

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/charlesvdv/google-hashcode-2020
 
 go 1.13
+
+require github.com/jinzhu/copier v0.0.0-20190924061706-b57f9002281a
--- qualification/main.go
+++ qualification/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"os"
 	"sort"
+
+	"github.com/jinzhu/copier"
 )
 
 type Library struct {
@@ -58,8 +60,8 @@ func NewProcess(days int, booksScores []int, libraries []Library) Process {
 
 func (proc *Process) Calculate() ([]int, map[int][]int) {
 	for day := 0; day < proc.Days; day++ {
-		// fmt.Printf("%v %v %v\n", day, len(proc.NonProcessedLibraries), (proc.SignedUpLibrary))
-		proc.keepLibrarySignupGoing()
+		// // fmt.Printf("%v %v %v\n", day, len(proc.NonProcessedLibraries), (proc.SignedUpLibrary))
+		proc.keepLibrarySignupGoing(proc.Days - day)
 
 		for _, library := range proc.SignedUpLibrary {
 			proc.registerBook(library)
@@ -68,7 +70,8 @@ func (proc *Process) Calculate() ([]int, map[int][]int) {
 	return proc.SignUpLibrary, proc.BooksScannedByLib
 }
 
-func (proc *Process) keepLibrarySignupGoing() {
+func (proc *Process) keepLibrarySignupGoing(daysLeft int) {
+	// fmt.Printf("pending: %v\n", proc.PendingSignupLib)
 	if proc.PendingSignupLib != nil {
 		if proc.PendingSignupLib.SignupTime == 0 {
 			proc.SignUpLibrary = append(proc.SignUpLibrary, proc.PendingSignupLib.ID)
@@ -77,12 +80,53 @@ func (proc *Process) keepLibrarySignupGoing() {
 		}
 	}
 	if proc.PendingSignupLib == nil && len(proc.NonProcessedLibraries) != 0 {
-		proc.PendingSignupLib = &proc.NonProcessedLibraries[0]
-		proc.NonProcessedLibraries = proc.NonProcessedLibraries[1:]
+		// fmt.Printf("before %v\n", proc.NonProcessedLibraries)
+		bestLibIndex := proc.pickBestLibrary(daysLeft)
+		// fmt.Printf("index: %v\n", bestLibIndex)
+		pendingSignup := Library{}
+		//proc.PendingSignupLib = &proc.NonProcessedLibraries[bestLibIndex]
+		copier.Copy(&pendingSignup, &proc.NonProcessedLibraries[bestLibIndex])
+		proc.PendingSignupLib = &pendingSignup
+		// fmt.Printf("before delete: %v\n", proc.NonProcessedLibraries)
+		// fmt.Printf("pending signup: %v\n", proc.PendingSignupLib)
+		proc.NonProcessedLibraries = append(proc.NonProcessedLibraries[0:bestLibIndex], proc.NonProcessedLibraries[bestLibIndex+1:]...)
+		// fmt.Printf("after delete: %v\n", proc.NonProcessedLibraries)
+		// fmt.Printf("pending signup: %v\n", proc.PendingSignupLib)
 	}
 	if proc.PendingSignupLib != nil {
 		proc.PendingSignupLib.SignupTime -= 1
 	}
+}
+
+func (proc *Process) pickBestLibrary(daysLeft int) int {
+	bestLib := 0
+	bestScore := 0
+	for libraryIndex := range proc.NonProcessedLibraries {
+		library := proc.NonProcessedLibraries[libraryIndex]
+		daysLeftWithoutSignup := daysLeft - library.SignupTime
+		maxPossibleBooks := daysLeftWithoutSignup * library.BookCount
+		score := 0
+		bookTaken := 0
+		for bookIndex := 0; bookIndex < len(library.BookIDs); bookIndex++ {
+			if bookTaken > maxPossibleBooks {
+				break
+			}
+			bookID := library.BookIDs[bookIndex]
+			if proc.BookProcessed[bookID] {
+				continue
+			}
+			score += proc.BookScores[bookID]
+			bookTaken++
+		}
+		// fmt.Printf("%v\n", library)
+		score = score / library.SignupTime
+		if score > bestScore {
+			bestLib = libraryIndex
+			bestScore = score
+		}
+	}
+
+	return bestLib
 }
 
 func (proc *Process) registerBook(library Library) {
@@ -105,6 +149,7 @@ func (proc *Process) registerBook(library Library) {
 
 func main() {
 	days, books, libraries := parseInput(bufio.NewReader(os.Stdin))
+	// fmt.Printf("%v\n", libraries)
 
 	process := NewProcess(days, books, libraries)
 	libRegistered, booksByLib := process.Calculate()
